4bc1c9847869b31ae193263f2c3a33bd256965f5
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-04-01 16:44:14 +0200

    Populate select dropdown from database

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$host = 'localhost';
+$dbname = 'zoo';
+$user = 'zooAdmin';
+$password = 'zoo';
+
+$dbh = new PDO('mysql:host=localhost;dbname=zoo', $user, $password);
+
 if (isset($_POST['submit'])) {
-
-    $host = 'localhost';
-    $dbname = 'zoo';
-    $user = 'zooAdmin';
-    $password = 'zoo';
-
-    $dbh = new PDO('mysql:host=localhost;dbname=zoo', $user, $password);
 
     $query = "SELECT * FROM animals WHERE name LIKE CONCAT('%', :letter, '%') && category = :category ";
     $statement = $dbh->prepare($query, array(PDO::FETCH_ASSOC));
@@ -23,11 +23,25 @@ if (isset($_POST['submit'])) {
 
     $result = $statement->fetchAll();
 
-
+    // testing that query works. Will do actual rendering later
     foreach ($result as $animals) {
         echo $animals['name'] . "<br/>";
     }
 }
+// get an array from categories in order to populate the dropdown
+$query_category = "SELECT DISTINCT category FROM animals";
+
+$temp_categories = array();
+
+foreach ($dbh->query($query_category) as $category_result) {
+    array_push($temp_categories, $category_result['category']);
+}
+// get the array from outside the foreach loop
+$categories = $temp_categories;
+echo '<pre>';
+print_r($categories);
+echo '</pre>';
+
 
 ?>
 
@@ -47,10 +61,11 @@ if (isset($_POST['submit'])) {
         <input type="text" id="letter" name="letter">
         <label for="category">Choose a type of animal</label>
         <select name="category" id="category">
-            <option value="Däggdjur">Däggdjur</option>
-            <option value="Fisk">Fisk</option>
-            <option value="Fågel">Fågel</option>
-            <option value="Insekt">Insekt</option>
+            <!--  populated from the database -->
+            <option value="<?php echo $categories[0] ?>"><?php echo $categories[0] ?></option>
+            <option value="<?php echo $categories[1] ?>"><?php echo $categories[1] ?></option>
+            <option value="<?php echo $categories[2] ?>"><?php echo $categories[2] ?></option>
+            <option value="<?php echo $categories[3] ?>"><?php echo $categories[3] ?></option>
         </select>
         <input type="submit" name="submit" value="Submit">
     </form>
