b7d4f3e9c44f9b8260e008c92332d9ecfcf68bbc
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-04-09 11:01:22 +0200

    delete old file upload

--- results.php
+++ results.php
@@ -39,30 +39,7 @@ if (isset($_POST['submit'])) {
 
 <body>
     <h1>The Zoo</h1>
-    <p class="show-text">
-        <?php
-        if (isset($_POST["submit"])) {
-            $uploadDir = "uploads/";
-            $name = $_FILES['fileToUpload']['name'];
-            $uploadPath = $uploadDir . basename($name);
-            $temp = $_FILES['fileToUpload']['tmp_name'];
-            $fileName = pathinfo($name, PATHINFO_FILENAME);
-            $extension = pathinfo($name, PATHINFO_EXTENSION);
-            $counter = 1;
 
-            if (file_exists($uploadDir . $fileName . '.' . $extension)) {
-                $name = $fileName . '-' . $counter . '.' . $extension;
-                $uploadPath = $uploadDir . basename($name);
-                $counter++;
-            }
-
-            if (move_uploaded_file($temp, $uploadPath)) {
-                echo "File " . $name . " is uploaded!";
-            } else {
-                echo "There was an error uploading the file, please try again!";
-            }
-        }
-        ?></p>
     <div class="results-table-container">
         <a href="index.php">Go Back</a>
         <?php
